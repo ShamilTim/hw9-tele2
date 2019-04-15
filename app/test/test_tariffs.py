5636f639f14b2775a5bec3876df4337ba12429e2
ShamilTim <shamil.86bee@mail.ru>
2019-04-15 23:24:10 +0300

    Resolved #4

--- app/test/test_tariffs.py
+++ app/test/test_tariffs.py
@@ -1,31 +1,17 @@
 from app.tariffs import Tariff, TariffManager
 
 
-# class TestTariffManager:
-#    def test_actual(self):
-#        assert [Tariff('Мой разговор', price=190, gb=3, minutes=250)] == TariffManager.actual(TariffManager(self))
-
-
-# def test___init__(self):
-#    my_talk = Tariff('Мой разговор', price=190, gb=3, minutes=250)
-#    result = Tariff.__init__(self, name='Мой разговор', price=190, gb=3, minutes=250)
-#    assert my_talk == result
-
-
-def test_actual():
-    pass
-    # manager = TariffManager()
-    # my_talk = Tariff('Мой разговор', price=190, gb=3, minutes=250)
-    # manager.add(my_talk)
-    # actual = manager.actual()
-    # result = actual
-    # assert my_talk == result
-
-
-def test_archived():
-    pass
-    # manager = TariffManager()
-    # univer = Tariff('Универ', price=None, minutes_unlim_tele2=None, archived=True)
-    # manager.add(univer)
-    # result = manager.archived()
-    # assert univer == result
+def test_actual_and_archived():
+    manager = TariffManager()
+    my_talk = Tariff('Мой разговор', price=190, gb=3, minutes=250)
+    my_tele2 = Tariff('Мой Теле2', price=7, price_period='day', gb=6)
+    univer = Tariff('Универ', price=None, minutes_unlim_tele2=None, archived=True)
+    my_start = Tariff('Мой старт', price=99, gb=2, minutes=100, minutes_unlim_tele2=False, archived=True)
+    manager.add(my_talk)
+    manager.add(my_tele2)
+    manager.add(univer)
+    manager.add(my_start)
+    actual = manager.actual()
+    archived = manager.archived()
+    assert [my_talk, my_tele2] == actual
+    assert [univer, my_start] == archived
